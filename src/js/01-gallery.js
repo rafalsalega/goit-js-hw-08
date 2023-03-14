df9b21b36f73b7819b3f76bddbfe5f694345990d
rafalsalega <salegarafal@gmail.com>
2023-03-15 00:09:19 +0100

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,22 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-// import simpleLightbox from 'simplelightbox';
+import simpleLightbox from 'simplelightbox';
 
-// const gallery = document.querySelector('.gallery')
-// const markup = galleryItems
-//   .map(
-//     image => `<a class="gallery__item" href=${image.original}>
-//   <img class="gallery__image" src=${image.preview} alt="${image.description}" />
-// </a>`
-//   )
-//   .join('');
+const gallery = document.querySelector('.gallery')
+const markup = galleryItems
+  .map(
+    image => `<a class="gallery__item" href=${image.original}>
+  <img class="gallery__image" src=${image.preview} alt="${image.description}" />
+</a>`
+  )
+  .join('');
 
-// gallery.insertAdjacentHTML('beforeend', markup);
+gallery.insertAdjacentHTML('beforeend', markup);
 
-// (function () {
-//   new SimpleLightbox('.gallery a', {
-//     captionDelay: 250,
-//     captionsData: 'alt',
-//   });  
-// })();
+(function () {
+  new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+  });  
+})();
