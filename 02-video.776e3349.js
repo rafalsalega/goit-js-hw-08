9a57dba78e8f51d703526af8f8dd2a6cf9361323
rafalsalega <105451060+rafalsalega@users.noreply.github.com>
2023-03-15 21:01:50 +0000

    Deploying to gh-pages from @ rafalsalega/goit-js-hw-08@0d4de0d96f5631baa5999c502cab606b8429ecbd 🚀

--- 02-video.776e3349.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Vimeo.Player(e),r=localStorage.getItem("videoplayer-current-time"),o=_.throttle((function(e){localStorage.setItem("videoplayer-current-time",e.seconds)}),1e3);t.on("timeupdate",o),t.setCurrentTime(r);
-//# sourceMappingURL=02-video.776e3349.js.map
--- 02-video.bbac7937.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e),r=localStorage.getItem("videoplayer-current-time"),o=_.throttle((function(e){localStorage.setItem("videoplayer-current-time",e.seconds)}),1e3);t.on("timeupdate",o),t.setCurrentTime(r)}();
-//# sourceMappingURL=02-video.bbac7937.js.map
